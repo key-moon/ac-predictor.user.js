1e01226307445ae5659fdb99f5eb4148a9ce4bd9
keymoon <kymn0116@gmail.com>
2019-06-29 19:01:51 +0900

    [fix] userscript header

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 // ==UserScript==
-// @name        ac-predictor
+// @name        ac-predictor-minimal
 // @namespace   http://ac-predictor.azurewebsites.net/
-// @version     1.2.5
-// @description コンテスト中にAtCoderのパフォーマンスを予測します
+// @version     1.0.0
+// @description AtCoderのパフォーマンスを予測し、順位表に表示します。
 // @author      keymoon
 // @license     MIT
-// @require     https://greasyfork.org/scripts/386715-atcoder-sidemenu/code/atcoder-sidemenu.js
 // @require     https://greasyfork.org/scripts/386712-atcoder-userscript-libs/code/atcoder-userscript-libs.js
 // @supportURL  https://github.com/key-moon/ac-predictor.user.js/issues
 // @match       https://atcoder.jp/*/standings
